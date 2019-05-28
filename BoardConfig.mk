46e6ff424d6d14cf221c309a70b1060e9c7aa5b9
Felix <google@ix5.org>
2019-05-28 14:05:51 +0200

    joan: sepolicy: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    
    Change-Id: I046282b2a2e8c541726fb29cb0044503322d4be9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -205,7 +205,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
 
