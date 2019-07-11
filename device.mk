8121a5c7abb0d0ff3866d8a934aefbb22e71d078
Sujin Panicker <spanic@codeaurora.org>
2019-07-11 15:52:26 +0530

    joan: Declare low-latency and pro audio capability
    Declare low-latency and pro audio capability in device
    make files to indicate low-latency and pro audio support
    exists
    
    Change-Id: I0b10d74db354889069ecadfdf4f03857a0d6506b

--- device.mk
+++ device.mk
@@ -80,6 +80,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+PRODUCT_COPY_FILES += \
+    $(TOPDIR)frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
+    $(TOPDIR)frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2880
 TARGET_SCREEN_WIDTH := 1440
