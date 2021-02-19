b93e7ff910c8f86f891199c63788218f85510233
Bruno Martins <bgcngm@gmail.com>
2021-02-19 15:58:30 +0000

    joan: Use sed instead of patchelf to patch blobs
    Given that libhwbinder/libhidltransport have been combined into
    libhidlbase, we can make use (and abuse) of that fact when patching
    old blobs. Do it in all possible cases.
    
    Change-Id: I5ea6ca1ddf05b43123a2dcfc9e4cb1c68b15a696

--- extract-files.sh
+++ extract-files.sh
@@ -70,10 +70,11 @@ function blob_fixup() {
         ${PATCHELF} --add-needed libprocessgroup.so "${2}"
         ;;
     vendor/lib/hw/camera.msm8998.so)
-        sed -i "s/libsensor.so/libtensor.so/g" "${2}"
-        ${PATCHELF} --replace-needed libui.so libui_shim.so "${2}"
-        ${PATCHELF} --replace-needed libgui.so libgui_vendor.so "${2}"
-        ${PATCHELF} --remove-needed libandroid.so "${2}"
+        sed -i "s/libsensor\.so/libtensor\.so/g" "${2}"
+        sed -i "s/libhidltransport\.so/libgui_vendor\.so\x00\x00\x00/g" "${2}"
+        sed -i "s/libandroid\.so/libui_shim\.so/g" "${2}"
+        ${PATCHELF} --remove-needed libgui.so "${2}"
+        ${PATCHELF} --remove-needed libui.so "${2}"
         ;;
     vendor/lib/libmpbase.so)
         ${PATCHELF} --remove-needed libandroid.so "${2}"
