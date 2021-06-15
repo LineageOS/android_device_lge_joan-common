2009b7e4cbfce144ced92f4f26d4e70749f48d5d
SupremMotu <francisd132435@gmail.com>
2021-06-14 23:11:47 -0700

    joan: bump perf HAL version

--- device.mk
+++ device.mk
@@ -384,7 +384,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.0.vendor
+    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
