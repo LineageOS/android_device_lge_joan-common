6f0b537f5c794a97839224a760b02c2d13726e05
Slowhy <we@jcts.pw>
2022-02-02 09:52:30 +0800

    joan: Uprev bluetooth audio HIDL impl to 2.1

--- device.mk
+++ device.mk
@@ -45,7 +45,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.1-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msm8998 \
