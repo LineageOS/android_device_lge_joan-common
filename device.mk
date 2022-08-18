73ea65b39e4263fe1217e81783977f4b371bd1ac
Han Sol Jin <hansol@hansol.ca>
2022-08-18 00:16:37 -0700

    joan: move hotword enrollment blobs to system_ext
    Also split hotword permissions to separate xml, to
    copy it to system_ext
    
    Change-Id: I41dafe696bed5926672ef09e36b33ab93b2a20d7

--- device.mk
+++ device.mk
@@ -388,7 +388,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/privapp-permissions-joan.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-joan.xml
+    $(DEVICE_PATH)/configs/privapp-permissions-joan.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-joan.xml \
+    $(DEVICE_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-hotword.xml
 
 # Power
 PRODUCT_PACKAGES += \
