301555a3be0ca33c50b2811bbc663b0bd18b0d0f
Han Sol Jin <hansol@hansol.ca>
2022-12-14 19:12:16 -0800

    joan: rootdir: mild cleanup
    - init.qti.fm.rc forgotten, readded
    - vendor.lge.power.rc forgotten, readded
    - init.baseband.proxy.rc moved to init folder
    - reorder Makefile based on alphabetical order of LOCAL_SRC_FILES
    
    Change-Id: Iec86d244a9bf2f9f6ae2e15f4e981a06ae7d8f5a

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -101,7 +101,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.baseband.proxy.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.baseband.proxy.rc
+LOCAL_SRC_FILES    := etc/init/init.baseband.proxy.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_SYSTEM_EXT_ETC)/init
 include $(BUILD_PREBUILT)
 
@@ -159,6 +159,22 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init/init.lge.vendor.services.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qti.fm.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init/init.qti.fm.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := vendor.lge.power.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init/vendor.lge.power.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
