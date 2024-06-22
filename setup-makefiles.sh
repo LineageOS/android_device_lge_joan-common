f48d200c50cf8d1cff829fc3df345bdb0eb9e2b1
Han Sol Jin <hansol@hansol.ca>
2024-06-22 12:54:21 -0700

    joan-common: add L01K to guards
    Change-Id: I07bcb60e44b89ef9654c9aca7063d29b6ad9d633

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "joan"
+write_headers "joan l01k"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
