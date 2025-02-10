c6e233f79eacae231ae3910f5945d0dd9043801b
Han Sol Jin <hansol@hansol.ca>
2025-02-09 16:58:36 -0800

    joan-common: migrate to common Lineage lights AIDL
    Change-Id: If86f71d91497f241d9d4bf7d6f1857db12e11ff9

--- joan-common.mk
+++ joan-common.mk
@@ -274,7 +274,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.lge
+    android.hardware.light-service.lineage
 
 # Live Display
 PRODUCT_PACKAGES += \
